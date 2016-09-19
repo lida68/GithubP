e63c3988eb8ffeb6a5e0bc012138b6b0f389ef92
Lida Sa <lida.safarzadeh@gmail.com>
2016-09-19 16:34:36 +0430

    add login code

--- app/src/main/java/com/example/lida/githubp/ItemDetailActivity.java
+++ app/src/main/java/com/example/lida/githubp/ItemDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
@@ -19,21 +20,25 @@ import android.view.MenuItem;
  */
 public class ItemDetailActivity extends AppCompatActivity {
 
+    private static final String TAG ="lida hi" ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
-
+        Log.i(TAG, "onCreate: ");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
+
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
